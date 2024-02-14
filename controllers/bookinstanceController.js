6ee593cadf6b29c3ac2778b9082ac8a4ad51b67f
AlejandroXVII <m.alejandro.xvii@gmail.com>
2024-02-14 06:19:38 -0400

    Enable delate option

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -94,12 +94,24 @@ exports.bookinstance_create_post = [
 
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED: BookInstance delete GET");
+	// Get details of book and all their books (in parallel)
+	const instance = await BookInstance.findById(req.params.id).exec();
+
+	if (instance === null) {
+		// No results.
+		res.redirect("/catalog/bookinstances");
+	}
+
+	res.render("bookinstance_delete", {
+		title: "Delete Book Instance",
+		instance: instance,
+	});
 });
 
 // Handle BookInstance delete on POST.
 exports.bookinstance_delete_post = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED: BookInstance delete POST");
+	await BookInstance.findByIdAndDelete(req.body.bookinstancerid);
+	res.redirect("/catalog/bookinstances");
 });
 
 // Display BookInstance update form on GET.
